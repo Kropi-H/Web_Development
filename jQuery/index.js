f059630a3248f9cd924ccff2c4569b5d8a384ed8
Honza Kropacek <kropacek.h@gmail.com>
2019-02-08 20:26:32 +0100

    push commit

--- jQuery/index.js
+++ jQuery/index.js
@@ -16,8 +16,19 @@ $("h1").click(function(){
   $("h1").css("color", "red");
 });
 
-for(var i = 0; i<5; i++ ){
-  document.querySelectorAll("button")[i].addEventListner("click", fucntion(){
-    document.querySelector("h1").style.color = "purple";
-  });
-}
+// for(var i = 0; i<5; i++ ){
+//   document.querySelectorAll("button")[i].addEventListener("click", function(){
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+$("button").click(function(){
+  $("h1").css("color", "purple");
+});
+// $("input").keypress(function(event){
+//   $("h1").text(event.key);
+// });
+
+$("button").on("click", function(){
+  $("h1").delay(1100).animate({opacity:1}). slideUp(). slideDown(). animate({opacity:0.5});
+});
